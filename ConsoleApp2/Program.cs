6dad3fa6e3119b981e2637c612b0f90f7eff2341
Antony <antua.froloff@gmail.com>
2023-03-05 15:48:19 +0300

    s

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SetArray(3, 5);
+        }
+
+        static void SetArray(int x, int y)
+        {
+            int[,] array = new int[x, y];
+        }
+    }
+}
